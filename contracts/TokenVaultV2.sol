0c88860854c6aeae67ca9bfd3f1636bc833d63e0
brandon <brandon@gametaverse.io>
2025-04-17 15:58:14 +0800

    chore: update

--- contracts/TokenVaultV2.sol
+++ contracts/TokenVaultV2.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -13,7 +13,7 @@ import "solady/src/utils/FixedPointMathLib.sol";
 
 import "./VerifySignLib.sol";
 import "./interface/ITokenVaultFactory.sol";
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract TokenVaultV2 is
     Initializable,
